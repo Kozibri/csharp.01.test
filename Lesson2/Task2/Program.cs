372b5b5e57a2d7ac2f52ac18dc5c235625b2995d
Kozibri <228privet2285@mail.ru>
2024-02-08 20:45:21 +0200

    Lesson2:Task2

---
+++ Lesson2/Task2/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+string name = "Aghanim";
+
+int age = 10000;
+
+System.Console.WriteLine($"Hello {name}! Your age is {age}");
+
+System.Console.WriteLine(age + " ");
+
+8 в 7 степени
+double result = Math.Pow(8, 7);
+System.Console.WriteLine(result);
+
+ 2 в 4 степени
+int a = 487;
+int y = a % 10;
+int x = a / 10 % 10;
+int score = 1;
+int count = 0;
+while (count < y)
+    {
+        score = score * x;
+        count++;
+    }
+System.Console.WriteLine(score);
+*/
+До тех пор, пока...
+int a = 487;
+int y = a % 10;
+int x = a / 10 % 10;
+int score = 1;
+for (int i =0; i < y; i++)
+    {
+        score *= x;
+    }
+System.Console.WriteLine(score);
